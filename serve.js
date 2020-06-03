8eaf91c6325fdc9b34af52deb7ef967cec72e006
bradleyham <bradleyjamesham@gmail.com>
2020-06-03 12:35:52 +1200

    add product page

--- serve.js
+++ serve.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url')
 
 const overview = fs.readFileSync(__dirname + '/templates/overview.html', 'utf-8');
 const product = fs.readFileSync(__dirname + '/templates/product.html', 'utf-8');
@@ -7,23 +8,37 @@ const card = fs.readFileSync(__dirname + '/templates/card.html', 'utf-8');
 const dataFile = fs.readFileSync(__dirname + '/dev-data/data.json', 'utf-8');
 const data = JSON.parse(dataFile)
 
-const cardHtml = data.map(item => {
-    let output = card.replace('{%emoji}', item.image);
+function replaceTemplate(temp, item) {
+    let output = temp.replace(/{%emoji}/g, item.image);
     output = output.replace('{%fruit}', item.productName);
     output = output.replace('{%quantity}', item.quantity);
-    output = output.replace('{%price}', item.price);
+    output = output.replace(/{%price}/g, item.price);
+    output = output.replace('{%description}', item.description);
+    output = output.replace('{%from}', item.from);
+    output = output.replace('{%nutrients}', item.nutrients);
+    output = output.replace('{%productId}', item.id);
     output = output.replace('{%organic}', item.organic ? 'Organic' : '')
-
     return output
+}
+
+const cardHtml = data.map(item => {
+    return replaceTemplate(card, item);
 })
 
 const overviewFinal = overview.replace('{%cards}', cardHtml)
 
+
+////// server  
+
+
 const server = http.createServer((req, res) => {
-    if (req.url === '/' || req.url === '/overview') {
+    const { query, pathname } = url.parse(req.url, true);
+
+    if (pathname === '/' || pathname === '/overview') {
         res.end(overviewFinal)
-    } else if (req.url === '/product') {
-        res.end(product)
+    } else if (pathname === '/product') {
+        const productPage = replaceTemplate(product, data[query.id])
+        res.end(productPage)
     } else {
         res.end('<h1>404 not found</h1>')
     }
